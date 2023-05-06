dd1fbff87b3b771ec8c1033c6525e3d4fa1c05cc
Gabriel Roger <122801172+GabrielRogerDelano@users.noreply.github.com>
2023-05-05 23:38:44 -0300

    aula 3

---
+++ ex002/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data e Hora</title>
+</head>
+<body>
+    <h1>Data e Hora</h1>
+    <?php 
+        date_default_timezone_set("America/Sao_Paulo");
+        echo "Hoje é dia ". date("d/M/Y");
+        echo "e são ". date("G:i:s");
+
+    ?>
+</body>
+</html>
