84c828cd4a6c2ae73abb4d5e39fac4a421bc4a02
Gabriel Roger <122801172+GabrielRogerDelano@users.noreply.github.com>
2023-05-07 17:23:22 -0300

    Create index.php

---
+++ ex004/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos primitivos</title>
+</head>
+<body>
+    <h1>Teste de tipos primitivos</h1>
+    <?php 
+        // 0x = hexadecimal ob = binario 0 = octal
+        // $num = 010;
+        // echo "O valor da variavel é $num";
+
+        // $v = "Gustavo";
+        // var_dump($v);
+    
+        // $num = (int) 3e2; 3 x 10(2) coercao 
+        // echo "o valor é $num";
+        // var_dump($num);
+
+    $num = (float) "950";
+    var_dump($num);
+    ?>
+</body>
+</html>
