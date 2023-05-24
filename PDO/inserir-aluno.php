10bc055e83ecee4a5207ce57a329aa4b745177bb
Gabriel Roger <122801172+GabrielRogerDelano@users.noreply.github.com>
2023-05-24 20:11:17 -0300

    criação da lista e inserir alunos

---
+++ PDO/inserir-aluno.php
@@ -0,0 +1,14 @@
+<?php 
+
+use Gabrielrogerdelano\Pdo\Domain\Model\Student;
+require 'vendor/autoload.php';
+
+$databasePath = __DIR__ . '/banco.sqlite';
+$pdo = new PDO('sqlite:' . $databasePath);
+
+$student = new Student(null, 'Gabriel ', new \DateTimeImmutable('2004-09-15'));
+
+$sqlInsert = "INSERT INTO students (name, birth_date) VALUES ('{$student->name()}', '{$student->birthDate()->format('Y-m-d')}');";
+
+var_dump($pdo->exec($sqlInsert));
+?>
---
+++ PDO/lista-alunos.php
@@ -0,0 +1,12 @@
+<?php 
+
+require 'vendor/autoload.php';
+
+$databasePath = __DIR__ . '/banco.sqlite';
+$pdo = new PDO('sqlite:' . $databasePath);
+
+$statement = $pdo->query('SELECT * FROM students;');
+$studentList = $statement->fetchAll();
+
+echo $studentList[0]['name'];
+?>
